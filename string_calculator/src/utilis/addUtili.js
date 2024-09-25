0ba83158cee9b557896a92ed972ac49509ca13c7
Sai Kumar K <>
2024-09-25 09:48:42 +0530

    feat: support new line delimiters between numbers

--- string_calculator/src/utilis/addUtili.js
+++ string_calculator/src/utilis/addUtili.js
@@ -1,8 +1,10 @@
 export const add = (numbers) => {
+  numbers = numbers.replace(/\\n/g, "\n");
   if (numbers === "") {
     return null;
   }
-  const numberArray = numbers.split(",");
+  const formattedNumbers = numbers.replace(/\n/g, ",");
+  const numberArray = formattedNumbers.split(",");
   if (numberArray.length === 1) {
     return parseInt(numbers, 10);
   }
