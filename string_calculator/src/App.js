ae44505b7577ff8422a0f0c0cbbeedc547c13a2a
Sai Kumar K <>
2024-09-25 09:17:41 +0530

    feat: added button, input components and styles

--- string_calculator/src/App.js
+++ string_calculator/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import CalculatorInput from "./components/input";
+import CalculatorButton from "./components/button";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleCalculate = () => {
+    setResult(input);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className="App-header">
+      <div className="calculator-container">
+        <CalculatorInput
+          input={input}
+          onInputChange={(e) => setInput(e.target.value)}
+        />
+        <CalculatorButton onClick={handleCalculate} name="Calculate" />
+        <CalculatorButton
+          onClick={() => {
+            setInput("");
+            setResult(null);
+          }}
+          name="Clear"
+        />
+      </div>
+      <p className="result">{result}</p>
     </div>
   );
 }
