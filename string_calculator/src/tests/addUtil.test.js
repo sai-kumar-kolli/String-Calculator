d97a7a0ad90ef5805b7747e8adfa4e0881e68e96
Sai Kumar K <>
2024-09-25 12:21:10 +0530

    test: add test for custom delimiter

--- string_calculator/src/tests/addUtil.test.js
+++ string_calculator/src/tests/addUtil.test.js
@@ -24,3 +24,8 @@ test("should handle new lines between numbers", () => {
   const result = add("1\n2,3");
   expect(result).toBe(6);
 });
+
+test("should support custom delimiters", () => {
+  const result = add("//;\n1;2");
+  expect(result).toBe(3);
+});
