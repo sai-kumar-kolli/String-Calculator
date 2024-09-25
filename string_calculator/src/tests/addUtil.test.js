1310b0661b3216fadb6ca45365a8235542ed723f
Sai Kumar K <>
2024-09-25 09:35:53 +0530

    test: add test for single number input

--- string_calculator/src/tests/addUtil.test.js
+++ string_calculator/src/tests/addUtil.test.js
@@ -4,3 +4,8 @@ test("should return null for an empty string", () => {
   const result = add("");
   expect(result).toBe(null);
 });
+
+test("should return the number itself for a single number input", () => {
+  const result = add("1");
+  expect(result).toBe(1);
+});
